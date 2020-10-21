7bf41de15c6bdc8c65ef82a32dfb2fe2f1399305
hasanhabsyi2011 <71365610+hasanhabsyi2011@users.noreply.github.com>
2020-10-21 10:49:13 +0700

    Add files via upload

---
+++ tugas20.js
@@ -0,0 +1,4 @@
+var mat = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable."
+
+console.log(mat.match("are"))
+console.log(mat.match("words"))
